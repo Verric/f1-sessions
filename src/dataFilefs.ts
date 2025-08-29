e09452ad0a4fdb17736a9082e925ec4c01adfcf4
verric <beijing-planes@hotmail.com>
2025-08-29 12:35:38 +1000

    cleaned up FS helper file

--- src/dataFilefs.ts
+++ src/dataFilefs.ts
@@ -1,48 +1,27 @@
-import { readFileSync } from "node:fs";
-import { writeFile } from "node:fs/promises";
+import { readFileSync, writeFileSync } from "node:fs";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
 import type { RaceResultsType, Schedule } from "./types.js";
 
-const SESSION_DATA_FILE = "schedule.json" as const;
+const SESSION_DATA_FILE = "schedule.json";
 const RACE_DATA_FILE = "race.json";
 
-export function readSessionData() {
-  try {
-    const data = readFileSync(SESSION_DATA_FILE, { encoding: "utf-8", flag: "r" });
-    const sessionData = JSON.parse(data);
-    return sessionData;
-  } catch (e) {
-    const message = e instanceof Error ? e.message : String(e);
-    console.log("Error reading session data file", message);
-  }
+const SESSION_DATA = fileURLToPath(import.meta.resolve(join("..", "data", SESSION_DATA_FILE)));
+const RACE_DATA = fileURLToPath(import.meta.resolve(join("..", "data", RACE_DATA_FILE)));
+
+export function readSessionDataOrThrow() {
+  const data = readFileSync(SESSION_DATA, { encoding: "utf-8" });
+  return JSON.parse(data) as Schedule;
 }
 
 export function readRaceDataOrThrow() {
-  try {
-    const data = readFileSync(RACE_DATA_FILE, { encoding: "utf-8", flag: "r" });
-    const raceData: RaceResultsType = JSON.parse(data);
-    return raceData;
-  } catch (e) {
-    const message = e instanceof Error ? e.message : String(e);
-    throw new Error(`Error reading race data file: ${message}`);
-  }
+  const data = readFileSync(RACE_DATA, { encoding: "utf-8" });
+  return JSON.parse(data) as RaceResultsType;
 }
 
-// export async function parseOrThrow(): Promise<Schedule> {
-//   const fileData = await readOrThrow();
-//   let result: Schedule = [];
-//   try {
-//     result = ScheduleSchema.parse(JSON.parse(fileData));
-//   } catch (error) {
-//     if (error instanceof ZodError) {
-//       console.error("Problems validating file data file: ", JSON.stringify(error.issues, null, 2));
-//     }
-//   }
-//   return result;
-// }
-
-export async function save(data: Schedule) {
-  await writeFile(SESSION_DATA_FILE, JSON.stringify(data), {
-    encoding: "utf8",
+export function save(data: Schedule) {
+  writeFileSync(SESSION_DATA, JSON.stringify(data), {
+    encoding: "utf-8",
     flag: "a",
   });
 }
